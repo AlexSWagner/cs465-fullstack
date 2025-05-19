bc42ac4d7866cdaac1808cc35e9ae78112dcbdc6
Alex Wagner <alexander.wagner@snhu.edu>
2025-05-19 10:28:51 -0400

    Fixed navigation links to use Express routes

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,4 +1,7 @@
 /* GET home page */
 module.exports.index = function(req, res) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { 
+        title: 'Travlr Getaways',
+        home: true
+    });
 }; 
