655f41d83be88a62a8206b627a8eda0d139a6913
Alex Wagner <alexander.wagner@snhu.edu>
2025-06-22 10:43:50 -0400

    Module 7 Part 2: Secure API endpoints and prepare Angular authentication models

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,18 +1,42 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 
 const tripsController = require('../controllers/trips');
 const authController = require('../controllers/authentication');
 
+// Method to authenticate our JWT
+function authenticateJWT(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    if (authHeader == null) {
+        return res.sendStatus(401); // Unauthorized
+    }
+
+    // The header format is "Bearer <token>"
+    const token = authHeader.split(' ')[1];
+
+    if (token == null) {
+        return res.sendStatus(401); // Unauthorized
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) {
+            return res.sendStatus(401); // Unauthorized if token is no longer valid
+        }
+        req.user = user;
+        next(); // Token is valid, proceed to the controller
+    });
+}
+
 router
     .route('/trips')
     .get(tripsController.tripsList)
-    .post(tripsController.tripsAddTrip);
+    .post(authenticateJWT, tripsController.tripsAddTrip);
 
 router
     .route('/trips/:tripCode')
     .get(tripsController.tripsFindByCode)
-    .put(tripsController.tripsUpdateTrip);
+    .put(authenticateJWT, tripsController.tripsUpdateTrip);
 
 router.route('/register').post(authController.register);
 router.route('/login').post(authController.login);
