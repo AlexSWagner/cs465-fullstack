2bf3c95bd852761fdf328f92bab3eca9a57a7197
Alex Wagner <alexander.wagner@snhu.edu>
2025-06-22 10:26:22 -0400

    Module 7 Part 1: Implement complete back-end authentication system

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+require('dotenv').config();
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var hbs = require('hbs');
+
+var passport = require('passport');
+require('./app_api/config/passport');
 
 // Bring in the database
 require('./app_api/models/db'); // Connect to DB
@@ -26,11 +31,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 // Enable CORS
 app.use('/api', (req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     next();
 });
@@ -55,6 +61,12 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+app.use((err, req, res, next) => {
+    if (err.name === 'UnauthorizedError') {
+        res.status(401).json({ "message": err.name + ": " + err.message });
+    }
 });
 
 module.exports = app;
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const tripsController = require('../controllers/trips');
+const authController = require('../controllers/authentication');
 
 router
     .route('/trips')
@@ -12,5 +13,8 @@ router
     .route('/trips/:tripCode')
     .get(tripsController.tripsFindByCode)
     .put(tripsController.tripsUpdateTrip);
+
+router.route('/register').post(authController.register);
+router.route('/login').post(authController.login);
 
 module.exports = router; 
